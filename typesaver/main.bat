b4ab08c1c0a1cbb7b304b529e1aac8328b9de1b2
Leon <132808925+Leon8326@users.noreply.github.com>
2025-03-31 10:14:53 +0200

    Update main.bat

--- typesaver/main.bat
+++ typesaver/main.bat
@@ -1,8 +1,10 @@
 @echo off
 setlocal
 
-rem Call VBScript to capture the key press
-for /f "delims=" %%i in ('cscript //nologo keyPress.vbs') do set key=%%i
+rem Call PowerShell script to capture the key press
+for /f "delims=" %%i in ('powershell -Command "Add-Type -TypeDefinition 'using System; using System.Runtime.InteropServices; public class KeyPress { [DllImport(\"user32.dll\")] public static extern short GetAsyncKeyState(int vKey); }'; [Console]::Write('Press any key...'); while ($true) { $key = [System.Console]::ReadKey($true); [Console]::WriteLine($key.Key); break }"') do set key=%%i
+
+rem Capture the key
 echo %key% >> keylog.txt
 
 pause
